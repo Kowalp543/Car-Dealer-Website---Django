5828ed3d9abcba43691cdcf64e296105af183d4a
Kowalp543 <piotr.kowalczyk1@op.pl>
2022-05-13 15:52:26 +0200

    filter

--- komis/urls.py
+++ komis/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
-from .views import SamochodCreateView
+from .views import SamochodCreateView, offer_img, export_csv
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.home),
-    path('search/', views.search, name='oferty'),
+    path('search/<int:price_min>/<int:price_max>/<str:marka>', views.search, name='oferty'),
     path('add/', SamochodCreateView.as_view(), name='samochod_add'),
+    path('export/', export_csv, name='export_csv'),
+    path('img/', offer_img, name='offer_img'),
 ]
 
 if settings.DEBUG:
--- komis/views.py
+++ komis/views.py
@@ -3,25 +3,52 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from komis.models import Samochod
 from django.core.paginator import Paginator
+import csv
+from django.shortcuts import redirect
 
 
 def home(request):
     return render(request, 'home.html')
 
 
-def search(request):
-    samochod_list = Samochod.objects.all()
-    paginator = Paginator(samochod_list, 10)
+def search(request, **kwargs):
+    if request.method == 'POST':
+        return redirect('oferty', marka=request.POST['marka'], price_min=request.POST['price_min'], price_max=request.POST['price_max'])
+
+    if kwargs['marka'] == 'all':
+        samochod_list = Samochod.objects.filter(cena__gte=kwargs['price_min'],
+                                                cena__lte=kwargs['price_max'])
+    else:
+        samochod_list = Samochod.objects.filter(cena__gte=kwargs['price_min'],
+                                                cena__lte=kwargs['price_max'],
+                                                marka__icontains=kwargs['marka'])
+    paginator = Paginator(samochod_list, 8)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'search.html',  {'page_obj': page_obj})
+
+    return render(request, 'search.html', {'page_obj': page_obj})
 
 
 def offer_img(request):
-    return HttpResponse(request, content_type="../media/{{samochod.image}}")
+    return HttpResponse(request, content_type='../media/{{samochod.image}}')
 
 
 class SamochodCreateView(CreateView):
     model = Samochod
     fields = ('marka', 'model', 'przebieg', 'rok_produkcji', 'moc', 'pojemnosc', 'cena', 'paliwo', 'image')
+
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+
+    writer = csv.writer(response)
+    writer.writerow(['marka', 'model', 'przebieg', 'rok_produkcji', 'moc', 'pojemnosc', 'cena', 'paliwo'])
+
+    for samochod in Samochod.objects.all().values_list('marka', 'model', 'przebieg', 'rok_produkcji', 'moc',
+                                                       'pojemnosc', 'cena', 'paliwo'):
+        writer.writerow(samochod)
+
+    response['Content-Disposition'] = 'attachment; filename="samochod.csv"'
+
+    return response
